3bf9b54b9fb714e377e71e44d815a61445d30ade
Nikhil Thapa <tnikhil.official@gmail.com>
2023-06-28 23:40:45 +0545

    [middlewares] : Added middleware for authentication and authorization on protected routes.

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,3 +1,4 @@
 module.exports = function (req, res, next) {
-  
+  if (!req.user.isAdmin) return req.status(403).send('Access Forbidden.');
+  next();
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,15 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 module.exports = function (req, res, next) {
-  
+  const token = req.header('x-auth-token');
+  if (!token) return res.status(401).send('No Token. Access Denied');
+
+  try {
+    decoded = jwt.verify(token, process.env.KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).send('Invalid Token.' + err.message);
+  }
 };
